7f74dedebcf1f3af952b23e8bc35ca1a0b22a4e6
Lucas Thaynan <34990294+lucasthaynan@users.noreply.github.com>
2024-11-28 13:06:50 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,15 +3,16 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.0/firebase
 import { getDatabase, ref, set, onValue } from "https://www.gstatic.com/firebasejs/9.0.0/firebase-database.js";
 
 // Configuração do Firebase usando variáveis de ambiente
+// main.js
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    apiKey: "API_KEY_PLACEHOLDER",
+    authDomain: "AUTH_DOMAIN_PLACEHOLDER",
+    projectId: "PROJECT_ID_PLACEHOLDER",
+    storageBucket: "STORAGE_BUCKET_PLACEHOLDER",
+    messagingSenderId: "MESSAGING_SENDER_ID_PLACEHOLDER",
+    appId: "APP_ID_PLACEHOLDER",
+    measurementId: "MEASUREMENT_ID_PLACEHOLDER",
+    databaseURL: "DATABASE_URL_PLACEHOLDER"
 };
 
 // Inicializando o Firebase
